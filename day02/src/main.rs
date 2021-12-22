6a55dd49e30355ccbcf293d9f2f2fdb61a5615ed
Robert Plant <rob@robertplant.io>
2021-12-22 21:46:17 +0000

    day2 part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -26,12 +26,44 @@ fn traverse(input: &'static str) -> usize {
     (distance * depth).try_into().unwrap()
 }
 
+fn traverse_p2(input: &'static str) -> usize {
+    let mut distance = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in input.split("\n") {
+        let mut key_value = line.split(" ");
+        let operator = key_value.next().unwrap();
+        let value = key_value.next().unwrap().parse::<i32>().unwrap_or(0);
+
+        match operator {
+            "forward" => {
+                distance = distance + value;
+                depth = (aim * value) + depth;
+            }
+            "up" => {
+                aim = aim - value;
+            }
+            "down" => {
+                aim = aim + value;
+            }
+            _ => (),
+        };
+    }
+
+    (distance * depth).try_into().unwrap()
+}
+
 fn main() {
     let input_data = input::get_input();
 
     let total = traverse(input_data);
 
     println!("Found {:?}", total);
+
+    let total = traverse_p2(input_data);
+
+    println!("Found P2 {:?}", total);
 }
 
 #[cfg(test)]
@@ -50,6 +82,21 @@ down 8
 forward 2"
             ),
             150
+        )
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(
+            traverse_p2(
+                "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+            ),
+            900
         )
     }
 }
