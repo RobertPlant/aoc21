7b3e203f6a8b3e848a070da465a58dd998da8dbd
Robert Plant <rob@robertplant.io>
2021-12-22 21:04:32 +0000

    day1 part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -15,12 +15,38 @@ fn check_increment(input: Vec<isize>) -> isize {
     increase_count
 }
 
+fn check_increment_p2(input: Vec<isize>) -> isize {
+    let mut last3 = 99999;
+    let mut last2 = 99999;
+    let mut last1 = 99999;
+    let mut increase_count = 0;
+
+    for depth in input {
+        let current = depth + last2 + last1;
+        let previous = last3 + last2 + last1;
+
+        if current > previous {
+            increase_count = increase_count + 1;
+        }
+
+        last3 = last2;
+        last2 = last1;
+        last1 = depth;
+    }
+
+    increase_count
+}
+
 fn main() {
     let input_data = input::get_input();
 
-    let total = check_increment(input_data);
+    let total = check_increment(input_data.clone());
 
     println!("Found {:?}", total);
+
+    let total_p2 = check_increment_p2(input_data.clone());
+
+    println!("Found P2 {:?}", total_p2);
 }
 
 #[cfg(test)]
@@ -32,6 +58,14 @@ mod tests {
         assert_eq!(
             check_increment(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
             7
+        )
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(
+            check_increment_p2(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+            5
         )
     }
 }
