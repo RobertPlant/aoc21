301c7f6af2797bbac50e6d231c58f08712ace8eb
Robert Plant <rob@robertplant.io>
2022-01-25 20:21:02 +0000

    day7 part2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -13,11 +13,40 @@ fn calc(input: &'static str) -> usize {
     parsed.iter().map(|v| max(median, v) - min(median, v)).sum()
 }
 
+fn calc_p2(input: &'static str) -> usize {
+    let parsed: Vec<usize> = input
+        .split(",")
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect();
+    let sum = (parsed.clone().iter().sum::<usize>()) as f32;
+    let len = (parsed.len()) as f32;
+    let mean = ((sum / len).round()) as usize;
+
+    min(
+        get_fuel_cost(&parsed, mean - 1),
+        min(
+            get_fuel_cost(&parsed, mean),
+            get_fuel_cost(&parsed, mean + 1),
+        ),
+    )
+}
+
+fn get_fuel_cost(movement: &Vec<usize>, mean: usize) -> usize {
+    movement
+        .iter()
+        .map(|v| {
+            let distance = max(mean, *v) - min(mean, *v);
+
+            (distance * (distance + 1)) / 2
+        })
+        .sum::<usize>()
+}
+
 fn main() {
     let input_data = input::get_input();
 
     println!("Found {:?}", calc(input_data));
-    //println!("Found P2 {:?}", calc_p2(input_data, 256));
+    println!("Found P2 {:?}", calc_p2(input_data));
 }
 
 #[cfg(test)]
@@ -27,5 +56,10 @@ mod tests {
     #[test]
     fn test() {
         assert_eq!(calc("16,1,2,0,4,2,7,1,2,14"), 37)
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(calc_p2("16,1,2,0,4,2,7,1,2,14"), 168)
     }
 }
