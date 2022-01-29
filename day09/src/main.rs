d8a96df0fd973600f669c4512cc82780882449b3
Robert Plant <rob@robertplant.io>
2022-01-29 20:56:38 +0000

    day9 part2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,5 +1,149 @@
 mod input;
 
+fn calc_p2(input: &'static str) -> usize {
+    let values: Vec<Vec<usize>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect::<Vec<usize>>()
+        })
+        .collect();
+    let height = values.len() - 1;
+    let width = values[0].len() - 1;
+    let mut basins = vec![];
+
+    for (row_i, row) in values.iter().enumerate() {
+        for (column_i, column) in row.iter().enumerate() {
+            let mut found = true;
+
+            if row_i != 0 && values[row_i - 1][column_i] <= *column {
+                found = false;
+            }
+
+            if row_i != height && values[row_i + 1][column_i] <= *column {
+                found = false;
+            }
+
+            if column_i != 0 && values[row_i][column_i - 1] <= *column {
+                found = false;
+            }
+
+            if column_i != width && values[row_i][column_i + 1] <= *column {
+                found = false;
+            }
+
+            if found {
+                basins.push(find_basin_size(&values, row_i, column_i, &mut vec![]) - 1);
+            }
+        }
+    }
+
+    basins.sort();
+    let len = basins.len();
+
+    basins[(len - 3)..len].to_vec().iter().product()
+}
+
+fn find_basin_size(
+    values: &Vec<Vec<usize>>,
+    row_i: usize,
+    column_i: usize,
+    found: &mut Vec<(usize, usize)>,
+) -> usize {
+    let mut size = 1;
+    let has_upper = row_i != 0;
+    let has_lower = row_i != values.len() - 1;
+    let has_left = column_i != 0;
+    let has_right = column_i != values[0].len() - 1;
+    let value = *values
+        .iter()
+        .nth(row_i)
+        .unwrap()
+        .iter()
+        .nth(column_i)
+        .unwrap();
+    let new_value = value + 1;
+    if new_value == 9 {
+        return size;
+    }
+
+    size += if has_upper && !found.contains(&(row_i - 1, column_i)) {
+        if *values
+            .iter()
+            .nth(row_i - 1)
+            .unwrap()
+            .iter()
+            .nth(column_i)
+            .unwrap()
+            != 9
+        {
+            found.push((row_i - 1, column_i));
+            find_basin_size(&values, row_i - 1, column_i, found)
+        } else {
+            0
+        }
+    } else {
+        0
+    };
+    size += if has_lower && !found.contains(&(row_i + 1, column_i)) {
+        if *values
+            .iter()
+            .nth(row_i + 1)
+            .unwrap()
+            .iter()
+            .nth(column_i)
+            .unwrap()
+            != 9
+        {
+            found.push((row_i + 1, column_i));
+            find_basin_size(&values, row_i + 1, column_i, found)
+        } else {
+            0
+        }
+    } else {
+        0
+    };
+    size += if has_left && !found.contains(&(row_i, column_i - 1)) {
+        if *values
+            .iter()
+            .nth(row_i)
+            .unwrap()
+            .iter()
+            .nth(column_i - 1)
+            .unwrap()
+            != 9
+        {
+            found.push((row_i, column_i - 1));
+            find_basin_size(&values, row_i, column_i - 1, found)
+        } else {
+            0
+        }
+    } else {
+        0
+    };
+    size += if has_right && !found.contains(&(row_i, column_i + 1)) {
+        if *values
+            .iter()
+            .nth(row_i)
+            .unwrap()
+            .iter()
+            .nth(column_i + 1)
+            .unwrap()
+            != 9
+        {
+            found.push((row_i, column_i + 1));
+            find_basin_size(&values, row_i, column_i + 1, found)
+        } else {
+            0
+        }
+    } else {
+        0
+    };
+
+    size
+}
+
 fn calc(input: &'static str) -> usize {
     let values: Vec<Vec<usize>> = input
         .lines()
@@ -46,7 +190,7 @@ fn main() {
     let input_data = input::get_input();
 
     println!("Found {:?}", calc(input_data));
-    // println!("Found P2 {:?}", calc_p2(input_data));
+    println!("Found P2 {:?}", calc_p2(input_data));
 }
 
 #[cfg(test)]
@@ -120,6 +264,20 @@ mod tests {
 5899465670"
             ),
             29
+        )
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(
+            calc_p2(
+                "2199943210
+3987894921
+9856789892
+8767896789
+9899965678"
+            ),
+            1134
         )
     }
 }
