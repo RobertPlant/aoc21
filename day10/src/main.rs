24aee772a480eabdba5e0575d1ffff5456aad021
Robert Plant <rob@robertplant.io>
2022-02-01 20:24:30 +0000

    day10 part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -47,11 +47,90 @@ fn calc(input: &'static str) -> usize {
     score
 }
 
+fn calc_p2(input: &'static str) -> usize {
+    let mut invalid = vec![];
+
+    'outer: for line in input.lines() {
+        let mut prev = vec![];
+
+        for c in line.chars() {
+            let is_closer = vec!['}', ')', ']', '>'].contains(&c);
+
+            if is_closer {
+                let p = prev.pop().unwrap();
+                match c {
+                    ')' => {
+                        if p != '(' {
+                            continue 'outer;
+                        }
+                    }
+                    ']' => {
+                        if p != '[' {
+                            continue 'outer;
+                        }
+                    }
+                    '}' => {
+                        if p != '{' {
+                            continue 'outer;
+                        }
+                    }
+                    '>' => {
+                        if p != '<' {
+                            continue 'outer;
+                        }
+                    }
+                    _ => {
+                        prev.push(p);
+                    }
+                }
+            } else {
+                prev.push(c);
+            }
+        }
+
+        invalid.push(prev);
+    }
+
+    let len = invalid.len();
+    let mut scores: Vec<usize> = invalid
+        .iter()
+        .map(|item| {
+            item.iter().rev().fold(0, |accum, c| {
+                let mut score = accum;
+
+                score = score * 5;
+
+                match c {
+                    '(' => {
+                        score += 1;
+                    }
+                    '[' => {
+                        score += 2;
+                    }
+                    '{' => {
+                        score += 3;
+                    }
+                    '<' => {
+                        score += 4;
+                    }
+                    _ => (),
+                }
+
+                score
+            })
+        })
+        .collect::<Vec<usize>>();
+
+    scores.sort();
+
+    scores[len / 2]
+}
+
 fn main() {
     let input_data = input::get_input();
 
     println!("Found {:?}", calc(input_data));
-    // println!("Found P2 {:?}", calc_p2(input_data));
+    println!("Found P2 {:?}", calc_p2(input_data));
 }
 
 #[cfg(test)]
@@ -74,6 +153,25 @@ mod tests {
 <{([{{}}[<[[[<>{}]]]>[]]"
             ),
             26397
+        )
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(
+            calc_p2(
+                "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"
+            ),
+            288957
         )
     }
 }
