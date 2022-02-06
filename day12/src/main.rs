037ad8c84ff5be5acaf4f4e245e98c10735ac1ff
Robert Plant <rob@robertplant.io>
2022-02-06 19:29:59 +0000

    day12 part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -76,16 +76,67 @@ fn parse(input: &'static str) -> Vec<Node> {
 
     nodes
 }
+fn routes_p2(input: Vec<Node>) -> Vec<String> {
+    let mut routes = vec![];
+    let start_id = input.iter().position(|n| n.id == "start").unwrap();
+
+    for link in input[start_id].links.iter() {
+        let link = input.iter().nth(*link).unwrap();
+
+        delve_p2(
+            &input,
+            link,
+            &mut routes,
+            input[start_id].id.clone() + "," + &link.id + ",",
+        );
+    }
+
+    routes.sort();
+
+    routes
+}
+
+fn delve_p2(input: &Vec<Node>, target: &Node, routes: &mut Vec<String>, chain: String) {
+    for id in target.links.iter() {
+        let target = input.iter().nth(*id).unwrap();
+        let current_chain = chain.clone() + &target.id + ",";
+        let search = ",".to_owned() + &target.id + &",".to_owned();
+        let mut visited_small_already = false;
+        input
+            .iter()
+            .filter(|n| !n.big_cave && n.id != "end" && n.id != "start")
+            .for_each(|n| {
+                if !visited_small_already {
+                    let node_search_term = ",".to_owned() + &n.id + &",".to_owned();
+                    visited_small_already = chain.matches(&node_search_term).count() > 1;
+                }
+            });
+
+        if !target.big_cave && chain.contains(&search) && visited_small_already {
+            continue;
+        }
+
+        if target.id == "end" {
+            routes.push(chain.clone() + &target.id);
+        }
+
+        delve_p2(input, target, routes, current_chain);
+    }
+}
 
 fn calc(input: &'static str) -> usize {
     routes(parse(input)).len()
+}
+
+fn calc_p2(input: &'static str) -> usize {
+    routes_p2(parse(input)).len()
 }
 
 fn main() {
     let input_data = input::get_input();
 
     println!("Found {:?}", calc(input_data));
-    //println!("Found P2 {:?}", calc_p2(input_data, 1000));
+    println!("Found P2 {:?}", calc_p2(input_data));
 }
 
 #[derive(Debug, PartialEq)]
@@ -228,6 +279,41 @@ kj-HN
 kj-dc"
             ),
             19
+        )
+    }
+
+    #[test]
+    fn test_calc_p2() {
+        assert_eq!(
+            calc_p2(
+                "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"
+            ),
+            36
+        )
+    }
+
+    #[test]
+    fn test_calc_p2_2() {
+        assert_eq!(
+            calc_p2(
+                "dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"
+            ),
+            103
         )
     }
 }
